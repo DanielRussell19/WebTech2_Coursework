b7ee6673a937dab1c0fe21d47660a6abb7df98a1
DanielRussell <DRUSSE204@caledonian.ac.uk>
2020-04-17 17:21:36 +0100

    User object

--- Models/User.js
+++ Models/User.js
@@ -1,3 +1,16 @@
-class user{
-    username
+//defines a user object
+class User{
+    //variables
+    username = null;
+    password = null;
+    email = null;
+
+    //contructors
+    constructor(username,password,email){
+        this.username = username;
+        this.password = password;
+        this.email = email;
+    }
 }
+
+module.exports = User;
